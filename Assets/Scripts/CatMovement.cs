8b56d92a240fa1ab324c26eff211862315d721b3
Vahn <vahnkess@gmail.com>
2023-09-11 15:17:38 +0200

    Cat Movement + Scene Changes
    Added a raycast so the cat only jumps when it's on the ground. Also created a platform layer that all platforms should be on. Also made the cat a prefab. Also combined the animation and movement files into the movement script.

--- Assets/Scripts/CatMovement.cs
+++ Assets/Scripts/CatMovement.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CatMovement : MonoBehaviour
 {
+    //=======Movement Variables========
     public float speed;
     private Rigidbody2D rb2d;
     public float jumpForce;
     private SpriteRenderer spriteRenderer;
+    public float raycastLength = 0.4f;
+    public LayerMask platformLayerMask;
+
+    //==========Animation Variables=======
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +25,30 @@ public class CatMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //=======Animation Code========
+        if (Input.GetKey(KeyCode.G))
+        {
+            //isItching = switcheroo(isItching);
+            animator.SetTrigger("Lick2");
+            //animator.SetBool("Lick2", false);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+
+            //animator.SetBool("Itch", false);
+        }
+
+
+        //========Movement Code=======
         Vector2 vel = rb2d.velocity;
         Vector2 normalizedDir = rb2d.velocity.normalized;
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            vel.y = jumpForce;
+            if (IsGround())
+            {
+                vel.y = jumpForce;
+            }
         }
         vel.x = Input.GetAxis("Horizontal") * speed;
 
@@ -35,6 +60,20 @@ public class CatMovement : MonoBehaviour
         else if (vel.x < -0.01)
         {
             spriteRenderer.flipX = true;
+        }
+    }
+
+    bool IsGround()//returns true if there is a platform below, false if not
+    {
+        Vector2 origin = transform.position;
+        RaycastHit2D platformDown = Physics2D.Raycast(origin, Vector2.down, raycastLength, platformLayerMask);
+        if (platformDown.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
