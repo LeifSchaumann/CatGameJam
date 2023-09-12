43d89ae1d186f2ceba8c766f1b21f48b3cd6bbde
Caroline Jareb <cjareb@gmail.com>
2023-09-12 14:11:45 +0200

    Added victory to sushi

--- Assets/Scripts/SushiEating.cs
+++ Assets/Scripts/SushiEating.cs
@@ -8,6 +8,8 @@ public class SushiEating : MonoBehaviour
     public int sushiEaten = 0;
     public int sashimiEaten = 0;
     public int rawEaten = 0;
+    public GameObject victoryText;
+    
     //public GameObject UI;
     //private Label scoreLabel;
     private AudioSource eatSound;
@@ -22,7 +24,10 @@ public class SushiEating : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (sushiEaten >= 1 && sashimiEaten >= 1 && rawEaten >= 1)
+        {
+            victoryText.GetComponent<SpriteRenderer>().enabled = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,21 +36,21 @@ public class SushiEating : MonoBehaviour
         {
             eatSound.Play();
             sushiEaten++;
-            //scoreLabel.text = sushiEaten.ToString() + "/3";
+            //scoreLabel.text = sushiEaten.ToString();
             Destroy(collision.gameObject);
         }
         if (collision.GetComponent<Sashimi>())
         {
             eatSound.Play();
             sashimiEaten++;
-            //scoreLabel.text = sashimiEaten.ToString() + "/3";
+            //scoreLabel.text = sashimiEaten.ToString();
             Destroy(collision.gameObject);
         }
         if (collision.GetComponent<RawFish>())
         {
             eatSound.Play();
-            sushiEaten++;
-            //scoreLabel.text = sushiEaten.ToString() + "/3";
+            rawEaten++;
+            //scoreLabel.text = sushiEaten.ToString();
             Destroy(collision.gameObject);
         }
     }
