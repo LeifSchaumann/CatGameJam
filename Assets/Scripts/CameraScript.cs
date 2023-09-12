679c59edd09b60ff92dcfff63759fd99143e25c1
Vahn <vahnkess@gmail.com>
2023-09-12 10:18:17 +0200

    Level 2 + Camera Movement
    Changed camera movement script. Updated level 2

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public float crazy_meter = 1;
+    public Transform player;
+    public float offsetX;
 
     // Start is called before the first frame update
     void Start()
@@ -13,8 +15,14 @@ public class CameraScript : MonoBehaviour
     }
 
     // locks camera's y
-    void LateUpdate() {
-        transform.position = new Vector3(transform.position.x, 1.15f, transform.position.z);
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = player.position.x + offsetX;
+        if (player.position.x >= offsetX)
+        {
+            transform.position = pos;
+        }
     }
 
     void goCrazy()
