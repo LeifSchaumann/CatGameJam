b052f1f0fab8fb08f1614c82c634ea07cf3fbf3c
CassidyOlson <cassidy.m.olson@gmail.com>
2023-09-12 11:57:10 +0200

    Button click animation

--- Assets/Scripts/ButtonMouseClick.cs
+++ Assets/Scripts/ButtonMouseClick.cs
@@ -7,8 +7,19 @@ public class ButtonMouseClick : MonoBehaviour
 
     public GameObject catPrefab;
 
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnMouseDown()
     {
+        //button animation
+        animator.SetTrigger("press");
+
+        //spawn cat
         Instantiate(catPrefab, new Vector3(-7, 5, 0), Quaternion.identity);
     }
 }
