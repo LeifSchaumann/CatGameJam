1b9e6b330255fa3bb8be8258ee9bab28708f1370
LeifSchaumann <schaumann1@kenyon.edu>
2023-09-12 14:28:59 +0200

    Sushi score counter

--- Assets/Scripts/SushiEating.cs
+++ Assets/Scripts/SushiEating.cs
@@ -8,24 +8,26 @@ public class SushiEating : MonoBehaviour
 {
     public int sushiEaten = 0;
     public int sashimiEaten = 0;
-    public int rawEaten = 0;
+    public int steakEaten = 0;
+    public GameObject UI;
     public GameObject victoryText;
-    
-    //public GameObject UI;
-    //private Label scoreLabel;
     private AudioSource eatSound;
+    Label sushiLabel;
+    Label sashimiLabel;
+    Label steakLabel;
     void Start()
     {
-        //VisualElement root = UI.GetComponent<UIDocument>().rootVisualElement;
-        //scoreLabel = root.Q<Label>("Score");
-
+        VisualElement root = UI.GetComponent<UIDocument>().rootVisualElement;
+        sushiLabel = root.Q<Label>("sushiScore");
+        sashimiLabel = root.Q<Label>("sashimiScore");
+        steakLabel = root.Q<Label>("steakScore");
         eatSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (sushiEaten >= 1 && sashimiEaten >= 1 && rawEaten >= 1)
+        if (sushiEaten > 1 && sashimiEaten > 1 && steakEaten >= 1)
         {
             victoryText.GetComponent<SpriteRenderer>().enabled = true;
             Invoke("ChangeScene", 2f);
@@ -38,21 +40,21 @@ public class SushiEating : MonoBehaviour
         {
             eatSound.Play();
             sushiEaten++;
-            //scoreLabel.text = sushiEaten.ToString();
+            sushiLabel.text = sushiEaten.ToString() + "/2";
             Destroy(collision.gameObject);
         }
         if (collision.GetComponent<Sashimi>())
         {
             eatSound.Play();
             sashimiEaten++;
-            //scoreLabel.text = sashimiEaten.ToString();
+            sashimiLabel.text = sashimiEaten.ToString() + "/2";
             Destroy(collision.gameObject);
         }
         if (collision.GetComponent<RawFish>())
         {
             eatSound.Play();
-            rawEaten++;
-            //scoreLabel.text = sushiEaten.ToString();
+            steakEaten++;
+            steakLabel.text = steakEaten.ToString() + "/2";
             Destroy(collision.gameObject);
         }
     }
